bec3b024e8ea355583e5202e33a367bc91471389
Yusuf Simpa <ojysimp@yahoo.com>
2022-04-08 09:01:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
-	 * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
