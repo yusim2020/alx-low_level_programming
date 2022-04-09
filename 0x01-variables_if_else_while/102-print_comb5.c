12e05c0259edf8d1c0eb7032639944dae0b6fe93
Yusuf Simpa <ojysimp@yahoo.com>
2022-04-09 07:23:57 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination
+ * Return: 0
+ */
+int main(void)
+{
+int digit1, digit2;
+
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2n = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar(digit1 % 10) + '0');
+putchar(digit2 % 10) + '0');
+
+if (digit1 == 8 && digit2 == 9)
+continue;
+
+putchar(',');
+putchar(' ");
+
+return (0);
+
