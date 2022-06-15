d3c4a9cee95007c57af6b18365b19983156b9eff
Yusuf Simpa <ojysimp@yahoo.com>
2022-06-15 15:57:42 -0700

    Singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_list - prints all strings in a linked list
- * @h: list parameter
- * Return: numbers of strongs encountered in the list
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t x;
 
-	while (h)
+	x = 0;
+	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		n++;
+		x++;
 	}
+	return (x);
 
-	return (n);
 }
